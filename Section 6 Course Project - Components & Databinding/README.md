1cc4dede178c6ffd3db5a171b14337305ef62bc8
Dxyk <danielkong1997@gmail.com>
2020-11-30 11:40:41 -0500

    Section 6 done

--- Section 6 Course Project - Components & Databinding/README.md
+++ Section 6 Course Project - Components & Databinding/README.md
@@ -1,3 +1,23 @@
 # Section 6 - Course Project - Components & Databinding
 
 ## Course Project
+
+### Lesson 84 - Introduction
+
+### Lesson 85 - Adding Navigation with Event Binding and ngIf
+
+Goal: Depend on the event emitted by the header, display either the recipe or the shopping list
+
+### Lesson 86 - Passing Recipe Data with Property Binding
+
+Goal: Extract Recipe List's items to RecipeListItem using property binding
+
+### Lesson 87 - Passing Data with Event and Property Binding (Combined)
+
+Goal: Click on an RecipeListItem and load the info into the RecipeDetail.
+
+### Lesson 88 - Make sure you have FormsModule added
+
+### Lesson 89 - Allowing the User to Add Ingredients to the Shopping List
+
+Goal: Add functionality to add new items in the shopping list
--- Section 6 Course Project - Components & Databinding/course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Section 6 Course Project - Components & Databinding/course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false })
+  nameInputRef: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  @ViewChild('amountInput', { static: false })
+  amountInputRef: ElementRef<HTMLInputElement>;
 
-  ngOnInit() {
+  @Output()
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onAddItem() {
+    this.ingredientAdded.emit({
+      name: this.nameInputRef.nativeElement.value,
+      amount: Number(this.amountInputRef.nativeElement.value),
+    });
   }
-
 }
