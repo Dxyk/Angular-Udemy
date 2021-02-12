1ea61e202b9a8f55ea7fa23cb867cd46550e3fd0
Dxyk <danielkong1997@gmail.com>
2021-02-11 20:12:54 -0500

    Lesson 218

--- Section 16 Course Project - Forms/README.md
+++ Section 16 Course Project - Forms/README.md
@@ -8,3 +8,18 @@ Goal: Use FormsModule to implement forms in
 
 - ShoppingList - Template Driven Approach
 - EditRecipes - Reactive Approach
+
+### Lesson 218 - TD: Adding the Shopping List Form
+
+Goal: Use TD approach to update the form in the shopping list edit component
+
+- Remove local references for inputs
+- Add event binding `ngSubmit`
+- Add local reference for form and assign it `ngForm`
+- Add `name` for input fields
+- Register input fields as control using `ngModel`
+
+In `shopping-edit.component.ts`
+
+- Update `onAddItem` to take in the `NgForm` object
+- Update logic to get the values using `NgForm.value`
--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../services/shopping-list.service';
 
@@ -8,17 +9,13 @@ import { ShoppingListService } from '../services/shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
-
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {}
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(formElement: NgForm) {
+    const value = formElement.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredient);
   }
 }
